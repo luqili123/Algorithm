a0fa6b9204f331347a9630265494eb26be40177e
lql <1036110216@qq.com>
2020-03-16 10:42:14 +0800

    背包

--- LeetCode/src/基础算法/背包问题.java
+++ LeetCode/src/基础算法/背包问题.java
@@ -1,5 +1,12 @@
 package 基础算法;
 
 public class 背包问题 {
-
+	public static void main(String[] args){
+		int[][] dp=new int[100][100];
+		int[] value=new int[10];
+		int[] w=new int[10];
+		int i=0;
+		int j=0;
+		dp[i][j]=Math.max(dp[i-1][j], dp[i-1][j-w[i]]+value[i]);
+	}
 }
